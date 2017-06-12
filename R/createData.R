f41670d8177be097136289c1f1645a568a60e87d
Davide Risso <dar2062@med.cornell.edu>
2017-06-12 16:56:06 -0400

    Changes to preprocessin of the data

--- R/createData.R
+++ R/createData.R
@@ -1,4 +1,17 @@
-load("../data/GSE95601_oeHBCdiff_Cufflinks_eSet")
+library(GEOquery)
+library(scone)
+if(!file.exists("../data/GSE95601_oeHBCdiff_Cufflinks_eSet.Rda")) {
+  download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE95601&format=file&file=GSE95601%5FoeHBCdiff%5FCufflinks%5FeSet%2ERda%2Egz",
+                "../data/GSE95601_oeHBCdiff_Cufflinks_eSet.Rda.gz")
+  gunzip("../data/GSE95601_oeHBCdiff_Cufflinks_eSet.Rda.gz")
+}
+load("../data/GSE95601_oeHBCdiff_Cufflinks_eSet.Rda")
+
+if(!file.exists("../data/oeHBCdiff_clusterLabels.txt")) {
+  download.file("https://raw.githubusercontent.com/rufletch/p63-HBC-diff/master/ref/oeHBCdiff_clusterLabels.txt",
+                "../data/oeHBCdiff_clusterLabels.txt")
+}
+
 ## count matrix
 E <- assayData(Cufflinks_eSet)$counts_table
 
@@ -22,15 +35,42 @@ bio <- droplevels(pData(Cufflinks_eSet)$MD_expt_condition)
 
 clusterLabels<-read.table("../data/oeHBCdiff_clusterLabels.txt",sep="\t",stringsAsFactors=FALSE)
 m<-match(colnames(E) ,clusterLabels[,1])
-metaData<-data.frame("Experiment"=bio,"Batch"=batch,"clusterLabels"=clusterLabels[m,2],qc)
+metadata<-data.frame("Experiment"=bio,"Batch"=batch,"clusterLabels"=clusterLabels[m,2],qc)
+
+# symbol for samples missing from original clustering
+metadata$clusterLabels[is.na(metadata$clusterLabels)] <- -2
+
+se <- SummarizedExperiment(assays = list(counts = E),
+                           colData = metadata)
+
+
+# Initial Gene Filtering
+num_reads = quantile(assay(se)[assay(se) > 0])[4]
+num_cells = 0.25*ncol(assay(se))
+is_common = rowSums(assay(se) >= num_reads ) >= num_cells
+table(is_common)
+
+# Metric-based Filtering sample-filtering
+data("housekeeping")
+hk = rownames(se)[toupper(rownames(se)) %in% housekeeping$V1]
+
+mfilt <- metric_sample_filter(assay(se), nreads = colData(se)$NREADS,
+                             ralign = colData(se)$RALIGN,
+                             pos_controls = rownames(se) %in% hk,
+                             zcut = 3, mixture = FALSE,
+                             plot = TRUE)
+
+# Simplify to a single logical
+mfilt <- !apply(simplify2array(mfilt[!is.na(mfilt)]),1,any)
+table(mfilt)
+
+se <- se[,mfilt]
 
 # filtering to top 1000 most variable:
 library(matrixStats)
-vars <- rowVars(log1p(E))
-names(vars) <- rownames(E)
+vars <- rowVars(log1p(assay(se)))
+names(vars) <- rownames(se)
 vars <- sort(vars, decreasing = TRUE)
-core <- E[names(vars)[1:1000],]
+core <- se[names(vars)[1:1000],]
 
-
-write.table(core,file="../data/oeCufflinkCountData_1000Var.txt",sep="\t",row.names=TRUE,col.names=TRUE)
-write.table(metaData,file="../data/oeMetadata.txt",sep="\t",row.names=TRUE,col.names=TRUE)
+save(core, file="../data/oe_se_1000Var.rda")
