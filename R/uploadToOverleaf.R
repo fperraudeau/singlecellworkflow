a1b72b45cdb3d74e79cd10781b8f9747b452d9fc
Fanny <fperraudeau@berkeley.edu>
2017-07-06 19:34:44 +0900

    temptatives to change colors in plotbarplot

--- R/uploadToOverleaf.R
+++ R/uploadToOverleaf.R
@@ -1,5 +1,4 @@
 library(BiocWorkflowTools)
 workflow_dir <- file.path(getwd(), 'workflow')
 uploadToOverleaf(files = workflow_dir,
-                 openInBrowser = TRUE,
-                 forceNewProject = T)
+                 openInBrowser = TRUE)
